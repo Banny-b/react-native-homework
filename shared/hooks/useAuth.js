fc52c789311bee5ff07b1dd61219139522daa75d
Sergey Petukhov <banny75@i.ua>
2023-06-06 15:31:47 +0300

    react-native
    Home work my-app

---
+++ shared/hooks/useAuth.js
@@ -0,0 +1,10 @@
+import { useContext } from "react";
+import { authContext } from "../AuthContext";
+
+const useAuth = () => {
+    const context = useContext(authContext);
+
+    return context;
+}
+
+export default useAuth;
